ae88d6502663094e84535f17a038ea60611b8a91
Güney <anilguneyaltun@gmail.com>
2021-07-08 02:45:52 +0300

    textures updated, ambient light added

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <cmath>
 #include <vector>
 
 #include <glew.h>
@@ -11,6 +14,8 @@
 #include "Mesh.h"
 #include "Shader.h"
 #include "Camera.h"
+#include "Texture.h"
+#include "Light.h"
 
 GLfloat dT = 0.0f;
 GLfloat lastTime = 0.0f;
@@ -21,6 +26,10 @@ GLWindow mainWindow;
 std::vector<Mesh*> meshList;
 std::vector<Shader> shaderList;
 Camera camera;
+Light mainLight;
+
+Texture brickTex;
+Texture dirtTexture;
 
 static const char *vShader = "../src/vshader.glsl";
 static const char *fShader = "../src/fshader.glsl";
@@ -28,59 +37,28 @@ static const char *fShader = "../src/fshader.glsl";
 void createObjects()
 {
     unsigned int indices[] = {
-            0,1,3,
-            3,1,2,
-            4,5,7,
-            7,5,6,
-            8,9,11,
-            11,9,10,
-            12,13,15,
-            15,13,14,
-            16,17,19,
-            19,17,18,
-            20,21,23,
-            23,21,22
+            0, 3, 1,
+            1, 3, 2,
+            2, 3, 0,
+            0, 1, 2
     };
 
     GLfloat vertices[] = {
-            -0.5f,0.5f,-0.5f,
-            -0.5f,-0.5f,-0.5f,
-            0.5f,-0.5f,-0.5f,
-            0.5f,0.5f,-0.5f,
-
-            -0.5f,0.5f,0.5f,
-            -0.5f,-0.5f,0.5f,
-            0.5f,-0.5f,0.5f,
-            0.5f,0.5f,0.5f,
-
-            0.5f,0.5f,-0.5f,
-            0.5f,-0.5f,-0.5f,
-            0.5f,-0.5f,0.5f,
-            0.5f,0.5f,0.5f,
-
-            -0.5f,0.5f,-0.5f,
-            -0.5f,-0.5f,-0.5f,
-            -0.5f,-0.5f,0.5f,
-            -0.5f,0.5f,0.5f,
-
-            -0.5f,0.5f,0.5f,
-            -0.5f,0.5f,-0.5f,
-            0.5f,0.5f,-0.5f,
-            0.5f,0.5f,0.5f,
-
-            -0.5f,-0.5f,0.5f,
-            -0.5f,-0.5f,-0.5f,
-            0.5f,-0.5f,-0.5f,
-            0.5f,-0.5f,0.5f
+            //	x      y      z			u	  v
+            -1.0f, -1.0f, 0.0f,		0.0f, 0.0f,
+            0.0f, -1.0f, 1.0f,		0.5f, 0.0f,
+            1.0f, -1.0f, 0.0f,		1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,		0.5f, 1.0f
     };
 
     Mesh *obj1 = new Mesh();
-    obj1->createMesh(vertices, indices, 256, 256);
+    obj1->createMesh(vertices, indices, 20, 12);
     meshList.push_back(obj1);
 
     Mesh *obj2 = new Mesh();
-    obj2->createMesh(vertices, indices, 12, 12);
+    obj2->createMesh(vertices, indices, 0, 0);
     meshList.push_back(obj2);
+
 }
 
 void createShaders(){
@@ -99,11 +77,20 @@ int main()
 
     camera = Camera(glm::vec3(0.0f,0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0), -90.0f, 0.0f, 3.0f, .05f);
 
-    GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0;
-    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 16.0f/9.0f, 0.3f, 1000.0f);
+    brickTex = Texture("/Users/guney/Developer/CLion/Textures/brick.png");
+    brickTex.loadTexture();
+    dirtTexture = Texture("/Users/guney/Developer/CLion/Textures/dirt.png");
+    dirtTexture.loadTexture();
+
+
+
+    GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformAmbientIntensity = 0, uniformAmbientColor = 0;
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (GLfloat)mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 100.0f);
+
 
     while (!mainWindow.getShouldClose())
     {
+        mainLight = Light(sin(glfwGetTime() + 0.5f), cos(glfwGetTime()+ 0.5f), sin(glfwGetTime() / 2), 1.0f);
         GLfloat currentTime = glfwGetTime();
         dT = currentTime - lastTime;
         lastTime = currentTime;
@@ -120,23 +107,28 @@ int main()
         uniformModel = shaderList[0].getModelLocation();
         uniformProjection = shaderList[0].getProjectionLocation();
         uniformView = shaderList[0].getViewLocation();
+        uniformAmbientColor = shaderList[0].getAmbientColorLocation();
+        uniformAmbientIntensity = shaderList[0].getAmbientIntensityLocation();
+
+        mainLight.useLight(uniformAmbientIntensity, uniformAmbientColor);
 
         glm::mat4 model(1.0f);
 
         model = glm::translate(model, glm::vec3(0.0f, 0.0f, -2.5f));
         model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
-        model = glm::rotate(model, (float)sin(glfwGetTime()) * 6.0f, glm::vec3(0.0f, 1.0f, 0.0f));
         glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
+        brickTex.useTexture();
         meshList[0]->renderMesh();
-/*
+
         model = glm::mat4(1.0f);
         model = glm::translate(model, glm::vec3(0.0f, 1.0f, -2.5f));
         model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
         glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+        dirtTexture.useTexture();
         meshList[1]->renderMesh();
-*/
+
         glUseProgram(0);
 
         mainWindow.swapBuffers();
